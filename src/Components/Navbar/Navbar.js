e032fdf6261bc84a4410ccbee972577dcb808701
Divyanshu-Dhakar <95903972+Divyanshu-Dhakar@users.noreply.github.com>
2023-04-03 15:28:16 +0530

    Update Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react';
 import './Navbar.css';
+import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai';
+import message from '../../Images/message.png'
 function Navbar() {
   const [showbutton,setShowButoon]=useState(false);
   return (
+    <nav className='sticky'>
     <div className='navbarbody'>
          <div className='linksparentbody'>
-             <h1><i class="fa-duotone fa-envelope"></i> Get in Touch</h1>
+             <h1> <img src={message}/> Get in Touch</h1>
             <div className='closeicon'>
             <AiOutlineClose style={{display : showbutton? 'block' : 'none' }} onClick={()=>{setShowButoon(false)}} className='closeicon'/>
             <AiOutlineMenu style={{display : showbutton? 'none' : 'block' }} onClick={()=>{setShowButoon(true)}} className='closeicon'/>
@@ -14,10 +17,12 @@ function Navbar() {
                </div>
             <div className={showbutton ? "showclass":"hideclass"}>
             <div className='navbarlinks'>
-              <h4>Home</h4>
-              <h4>About Us</h4>
-              <h4>Contact Us</h4>
-              <button>Linkedin</button>
+            <h4><a href='#home'>Home</a></h4>
+             <h4> <a href='#about'>About Me</a></h4>
+             <h4><a href='#services'>Services</a></h4>
+              <h4><a href='#portfolio'>Portfolio</a></h4>
+               <h4><a href='#blog'>Blogs</a></h4>
+              <button>Resume</button>
             </div>
          
               
@@ -25,6 +30,7 @@ function Navbar() {
           
    
     </div>
+    </nav>
   )
 }
 
